4c6268c4ceacd66723d68304b2f9a3280f6d0bd5
Ruan Ferraz <ruanlima155@gmail.com>
2025-11-12 10:43:19 -0300

    build: setup do projeto

---
+++ src/test/java/com/rfdev/desafio_cdc/DesafioCdnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfdev.desafio_cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCdnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
