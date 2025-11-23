a1cc647b1fc70b534add66382e4cc3404a4b65b6
Ruan Ferraz <ruanlima155@gmail.com>
2025-11-23 16:55:06 -0300

    refac: pais armazena seus estados no relacionamento

--- src/main/java/com/rfdev/desafio_cdc/pais/Pais.java
+++ src/main/java/com/rfdev/desafio_cdc/pais/Pais.java
@@ -1,9 +1,12 @@
 package com.rfdev.desafio_cdc.pais;
 
+import com.rfdev.desafio_cdc.estado.Estado;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -20,6 +23,9 @@ public class Pais {
     @Column(name = "nome", nullable = false, unique = true)
     private String nome;
 
+    @OneToMany(mappedBy = "pais", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Estado> estados = new ArrayList<>();
+
     @Deprecated
     public Pais() {
     }
@@ -27,5 +33,13 @@ public class Pais {
     public Pais(@NotBlank String nome) {
         this.nome = nome;
     }
-    
+
+    public void adicionarEstado(Estado estado) {
+        estados.add(estado);
+    }
+
+    public Boolean possuiEstados() {
+        return !estados.isEmpty();
+    }
+
 }
