a2d3609666493ee0e037698850c7241da4f55228
Ruan Ferraz <ruanlima155@gmail.com>
2025-11-23 17:02:44 -0300

    refac: usando entity manager e  removendo classes que nao são usadas

--- src/main/java/com/rfdev/desafio_cdc/categoria/cadastro/CadastroCategoriaController.java
+++ src/main/java/com/rfdev/desafio_cdc/categoria/cadastro/CadastroCategoriaController.java
@@ -1,9 +1,11 @@
 package com.rfdev.desafio_cdc.categoria.cadastro;
 
 import com.rfdev.desafio_cdc.categoria.Categoria;
-import com.rfdev.desafio_cdc.categoria.CategoriaRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CadastroCategoriaController {
 
-    private final CategoriaRepository categoriaRepository;
+    @PersistenceContext
+    private final EntityManager entityManager;
 
-    public CadastroCategoriaController(CategoriaRepository categoriaRepository) {
-        this.categoriaRepository = categoriaRepository;
+    public CadastroCategoriaController(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
     @PostMapping("api/categorias")
+    @Transactional
     public ResponseEntity<CadastroCategoriaResponse> cadastrar(@RequestBody @Valid CadastroCategoriaRequest request) {
         Categoria categoria = request.toModel();
-        categoriaRepository.save(categoria);
+        entityManager.persist(categoria);
         return ResponseEntity.ok(CadastroCategoriaResponse.of(categoria));
     }
 }
