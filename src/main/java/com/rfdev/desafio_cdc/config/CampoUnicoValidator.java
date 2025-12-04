259bc9e6f9b0709375f832813476472cd761c451
Ruan Ferraz <ruanlima155@gmail.com>
2025-12-04 14:59:32 -0300

    refac: usando query criteria para evitar sql injection

--- src/main/java/com/rfdev/desafio_cdc/config/CampoUnicoValidator.java
+++ src/main/java/com/rfdev/desafio_cdc/config/CampoUnicoValidator.java
@@ -2,7 +2,9 @@ package com.rfdev.desafio_cdc.config;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
@@ -27,12 +29,22 @@ public class CampoUnicoValidator implements ConstraintValidator<CampoUnico, Obje
             return true;
         }
 
-        Query query = entityManager.createQuery(
-                "SELECT COUNT(e) FROM " + nomeTabela.getSimpleName() + " e WHERE e." + nomeCampo + " = :value",
-                Long.class);
-        query.setParameter("value", value);
-        Long count = (Long) query.getSingleResult();
+        try {
+            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+            CriteriaQuery<Long> criteriaQuery = criteriaBuilder.createQuery(Long.class);
+            Root<?> root = criteriaQuery.from(nomeTabela);
 
-        return count == 0;
+            criteriaQuery.select(criteriaBuilder.count(root))
+                    .where(criteriaBuilder.equal(root.get(nomeCampo), value));
+
+            Long count = entityManager.createQuery(criteriaQuery).getSingleResult();
+
+            return count == 0;
+
+        } catch (IllegalArgumentException e) {
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
